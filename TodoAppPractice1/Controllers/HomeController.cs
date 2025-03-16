31f9d56f3eeb2fa9cf883930b9d26a06021b468b
Dhruv Mehta <dhruvmehta1311@gmail.com>
2025-03-16 12:35:43 +0530

    Add project files.

---
+++ TodoAppPractice1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAppPractice1.Models;
+
+namespace TodoAppPractice1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<TodoItems> todosList = new List<TodoItems>();
+        //{
+        //    //new TodoItems() { ID = 1, Task = "Learn new Topic", IsComplete = false },
+        //    //new TodoItems() { ID = 2, Task = "Sleep on Time", IsComplete = true },
+        //    //new TodoItems() { ID = 3, Task = "Try to learn a new skill.", IsComplete = false }
+        //};
+
+        private static int nextId = 0;
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(todosList);
+        }
+
+        [Route("add")]
+        public IActionResult Add(string task)
+        {
+
+            todosList.Add(new TodoItems() { ID = nextId++, Task = task, IsComplete = false });
+
+            return RedirectToAction("Index");
+        }
+
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var deleteItem = todosList.FirstOrDefault(x => x.ID == id);
+
+            if(deleteItem != null)
+            {
+                todosList.Remove(deleteItem);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ TodoAppPractice1/Models/TodoItems.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoAppPractice1.Models
+{
+    public class TodoItems
+    {
+        public int ID { get; set; }
+        public string? Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
